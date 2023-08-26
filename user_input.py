bac8422b36fbd232989baeb31c8efef7415efebe
wyfok <42815684+wyfok@users.noreply.github.com>
2023-08-26 16:18:30 +0200

    Update UI on gender + preferred foot

--- user_input.py
+++ user_input.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
 def user_input():
-    
+
     col1, col2 = st.columns(2)
     with col1:
         gender = st.selectbox(
-        'Gender',
-        ('Female','Male'))
+        'Gender',('Female','Male'))
+    with col2:
+        preferred_foot = st.selectbox(
+        'Preferred Foot',('Right','Left'))
+    col3, col4 = st.columns(2)
+    with col3:
         pace = st.slider('Select the value for Pace', 0, 100, 50)
         shooting = st.slider('Select the value for Shooting', 0, 100, 50)
         passing = st.slider('Select the value for Passing', 0, 100, 50)
-    with col2:
-        preferred_foot = st.selectbox(
-        'Preferred Foot',
-        ('Right','Left'))
+    with col4:
         dribbling = st.slider('Select the value for Dribbling', 0, 100, 50)
         defending = st.slider('Select the value for Defending', 0, 100, 50)
         physic = st.slider('Select the value for Physic', 0, 100, 50)
